af94abbbef4d3a1027ad8cfe9fc1b3f89a7a185d
Nail Sharipov <nailxsharipov@gmail.com>
2020-02-18 17:52:40 +0300

    add PathList api

---
+++ Runtime/iShape/Geometry/Container/PathList.cs
@@ -0,0 +1,48 @@
+using Unity.Collections;
+using UnityEngine;
+
+namespace iShape.Geometry.Container {
+
+    public struct PathList {
+        
+        public NativeArray<Vector2> points;
+        public NativeArray<PathLayout> layouts;
+        public int Count => layouts.Length;
+
+        public PathList(NativeArray<Vector2> points, NativeArray<PathLayout> layouts) {
+            this.points = points;
+            this.layouts = layouts;
+        }
+        
+        public PathList(Allocator allocator) {
+            this.points = new NativeArray<Vector2>(0, allocator);
+            this.layouts = new NativeArray<PathLayout>(0, allocator);
+        }
+
+        public PathList(NativeArray<Vector2> points, bool isClockWise, Allocator allocator) {
+            this.points = new NativeArray<Vector2>(points.Length, allocator);
+            this.points.CopyFrom(points);
+            this.layouts = new NativeArray<PathLayout>(1, allocator);
+            this.layouts[0] = new PathLayout(0, points.Length, isClockWise);
+        }
+
+        
+        public NativeArray<Vector2> Get(int index, Allocator allocator) {
+            var layout = this.layouts[index];
+            var array = new NativeArray<Vector2>(layout.length, allocator);
+            array.Slice(0, layout.length).CopyFrom(this.points.Slice(layout.begin, layout.length));
+            return array;
+        }
+        
+        public NativeSlice<Vector2> Get(int index) {
+            var layout = this.layouts[index];
+            return this.points.Slice(layout.begin, layout.length);
+        }
+
+        public void Dispose() {
+            this.points.Dispose();
+            this.layouts.Dispose();
+        }
+    }
+
+}
